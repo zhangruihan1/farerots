69d7454c6f6b0a1d4d11017d24e465bdb9feb649
zhangruihan1 <zrh15832168740@gmail.com>
2022-06-29 01:33:38 +0800

    add attack

--- blackbox_adversarial/objective.py
+++ blackbox_adversarial/objective.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-from image2perturbed import GlassesPerturbation
 from image2feature import get_feature, get_model
 
 def cosine_similarity_single_system(im1, im2, backbone_type, batched_1 = True, batched_2 = True):
@@ -19,19 +18,4 @@ def cosine_similarity_single_system(im1, im2, backbone_type, batched_1 = True, b
 	f1 = features[:im1.shape[0]]
 	f2 = features[im1.shape[0]:]
 
-	return cosine_similarity(f1, f2)	
-
-
-
-g1 = GlassesPerturbation()
-
-from PIL import Image
-i1 = np.array(Image.open('Screenshot 2022-06-28 213031.jpg').resize((112, 112)))
-i2 = g1.perturb_single_image(np.random.normal(128, 128, size = (10, g1.n_var)), i1)
-
-for i in range(10):
-	im = Image.fromarray(i2[i])
-	im.save(f"image_{i}.png")
-
-csss = cosine_similarity_single_system(i1, i2, 'EfficientNet')
-print(csss)
+	return cosine_similarity(f1, f2)
