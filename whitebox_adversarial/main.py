9bf81117ef2e88f7293682ef18e7b55ca1e84b04
Ubuntu <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-07-08 11:46:18 +0000

    gold

--- whitebox_adversarial/main.py
+++ whitebox_adversarial/main.py
@@ -14,47 +14,50 @@ from tqdm import trange
 
 # perturb = GlassesPerturbation()
 
+my_systems = ['r50', 'EfficientNet', 'ReXNet', 'AttentionNet', 'RepVGG', 'GhostNet', 'TF-NAS', 'LightCNN']
 
-box = WhiteboxWithMultipleSystems('r50')
-box2 = WhiteboxWithMultipleSystems('r50', 'EfficientNet', 'ReXNet')#, 'GhostNet', 'TF-NAS', 'LightCNN', 'RepVGG', 'AttentionNet')
+for my_system in my_systems:
+	print(my_system)
+	box = WhiteboxWithMultipleSystems(my_system)
+	box2 = WhiteboxWithMultipleSystems(*my_systems)
 
 
-from PIL import Image
+	from PIL import Image
 
-import csv
- 
-min_sims = []
+	import csv
+	
+	min_sims = []
 
-for k in trange(100):
+	for k in trange(100):
 
-	with open(f'sims_r50_{k}.csv', 'w', newline ='') as f:
+		with open(f'sims_{my_system}_{k}.csv', 'w', newline ='') as f:
 
-		write = csv.writer(f)
+			write = csv.writer(f)
 
-		i1 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_A.jpg").resize((112, 112))), 0) 
-		i2 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_B.jpg").resize((112, 112))), 0) 
+			i1 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_A.jpg").resize((112, 112))), 0) 
+			i2 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_B.jpg").resize((112, 112))), 0) 
 
-		i1_ = np.array(i2, copy=True)
+			i1_ = np.array(i2, copy=True)
 
-		min_sim = 1
+			min_sim = 1
 
-		for i in range(500):
-			g_1, _ = box.get_grads(i1_, i1)
-			i1_ = fgsm_attack(i1_, 1, g_1)
+			for i in range(100):
+				g_1, _ = box.get_grads(i1_, i1)
+				i1_ = fgsm_attack(i1_, 1, g_1)
 
-			delta = np.clip(i2 - i1_, -3, 3)
-			i1_ = np.clip(i2 + delta, 0, 255)
+				delta = np.clip(i2 - i1_, -3, 3)
+				i1_ = np.clip(i2 + delta, 0, 255)
 
-			sim_test = box2.cosine_similarity(i1_, i1)
+				sim_test = box2.cosine_similarity(i1_, i1)
 
-			if sim_test < min_sim:
-				min_sim = sim_test
-				print(box2.sims)
-				Image.fromarray(i1_.squeeze(0).astype(np.uint8)).save(f"../lfw/image_{k}_B_wblinf.png")
-				
-			write.writerow(box2.sims)
+				if sim_test < min_sim:
+					min_sim = sim_test
+					print(box2.sims)
+					Image.fromarray(i1_.squeeze(0).astype(np.uint8)).save(f"../lfw/image_{k}_B_wblinf.png")
+					
+				write.writerow(box2.sims)
 
-		min_sims.append(min_sim)
-		print('lowest similarity: ', min(min_sims))
+			min_sims.append(min_sim)
+			print('lowest similarity: ', min(min_sims))
 
-np.save(min_sims, 'min_sims')
+	np.save(my_system + '_min_sims', min_sims)
