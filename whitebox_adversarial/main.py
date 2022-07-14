1c3599e7fcc1329ffc7532a4fb3a75f91881f447
Ubuntu <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-07-14 17:05:04 +0000

    generate images from one system

--- whitebox_adversarial/main.py
+++ whitebox_adversarial/main.py
@@ -16,51 +16,106 @@ from tqdm import trange
 
 my_systems = ['FaceNet', 'r50', 'EfficientNet', 'ReXNet', 'AttentionNet', 'RepVGG', 'GhostNet', 'TF-NAS', 'LightCNN']
 
-for my_system in my_systems:
-	print(my_system)
-	box = WhiteboxWithMultipleSystems(my_system)
-	box2 = WhiteboxWithMultipleSystems(*my_systems)
+box = WhiteboxWithMultipleSystems('ReXNet')
+# box2 = WhiteboxWithMultipleSystems(*my_systems)
 
 
-	from PIL import Image
+from PIL import Image
 
-	import csv
+import csv
+
+min_y_preds = []
+
+for k in trange(6000):
+
+	# with open(f'y_preds_rex_{k}.csv', 'w', newline ='') as f:
+
+		# write = csv.writer(f)
+	if k%600 > 300:
+		continue
+
+	i1 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_A.jpg").resize((112, 112))), 0) 
+	i2 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_B.jpg").resize((112, 112))), 0) 
+
+	i1_ = np.array(i2, copy=True)
+
+	min_sim = 1
+
+	for i in range(100):
+		g_1, _ = box.get_grads(i1_, i1)
+		i1_ = fgsm_attack(i1_, 1, g_1)
+
+		delta = np.clip(i2 - i1_, -4, 4)
+		i1_ = np.clip(i2 + delta, 0, 255)
+
+		sim_test = box.cosine_similarity(i1_, i1)
+
+		if sim_test < min_sim:
+			min_sim = sim_test
+			Image.fromarray(i1_.squeeze(0).astype(np.uint8)).save(f"../lfw/image_{k}_B_wblinf.png")
+		if 0 in box.y_preds:
+			print(box.y_preds)
+			
+		write.writerow(box.y_preds)
+
+	min_y_preds.append(min_sim)
+	print('lowest similarity: ', min(min_y_preds))
+
+np.save('five' + '_min_y_preds', min_y_preds)
+
+# save verfied judgements
+
+# for my_system in my_systems:
+# 	print(my_system)
+# 	box = WhiteboxWithMultipleSystems(my_system)
+# 	box2 = WhiteboxWithMultipleSystems(*my_systems)
+
+
+# 	from PIL import Image
+
+# 	import csv
 	
-	min_y_preds = []
+# 	min_y_preds = []
 
-	for k in trange(100):
+# 	for k in trange(6000):
 
-		with open(f'y_preds_{my_system}_{k}.csv', 'w', newline ='') as f:
+# 		if k%600 > 300:
+# 			continue
 
-			write = csv.writer(f)
+# 		with open(f'y_preds_{my_system}_{k}.csv', 'w', newline ='') as f:
 
-			i1 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_A.jpg").resize((112, 112))), 0) 
-			i2 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_B.jpg").resize((112, 112))), 0) 
+# 			write = csv.writer(f)
 
-			i1_ = np.array(i2, copy=True)
+# 			i1 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_A.jpg").resize((112, 112))), 0) 
+# 			i2 = np.expand_dims(np.array(Image.open(f"../lfw/image_{k}_B.jpg").resize((112, 112))), 0) 
 
-			min_sim = 1
+# 			i1_ = np.array(i2, copy=True)
 
-			for i in range(100):
-				g_1, _ = box.get_grads(i1_, i1)
-				i1_ = fgsm_attack(i1_, 1, g_1)
+# 			min_sim = 1
 
-				delta = np.clip(i2 - i1_, -4, 4)
-				i1_ = np.clip(i2 + delta, 0, 255)
+# 			for i in range(1000):
+# 				g_1, _ = box.get_grads(i1_, i1)
+# 				i1_ = fgsm_attack(i1_, 1, g_1)
 
-				sim_test = box2.cosine_similarity(i1_, i1)
+# 				delta = np.clip(i2 - i1_, -4, 4)
+# 				i1_ = np.clip(i2 + delta, 0, 255)
 
-				if sim_test < min_sim:
-					min_sim = sim_test
-					print(box2.y_preds)
-					Image.fromarray(i1_.squeeze(0).astype(np.uint8)).save(f"../lfw/image_{k}_B_wblinf.png")
+# 				# sim_test = box2.cosine_similarity(i1_, i1)
+
+# 				# if sim_test < min_sim:
+# 				# 	min_sim = sim_test
+# 				# 	Image.fromarray(i1_.squeeze(0).astype(np.uint8)).save(f"../lfw/image_{k}_B_wblinf.png")
+# 				write.writerow(box.y_preds)
+# 				if 0 in box.y_preds:
+# 					break
 					
-				write.writerow(box2.y_preds)
 
-			min_y_preds.append(min_sim)
-			print('lowest similarity: ', min(min_y_preds))
+# 			min_y_preds.append(min_sim)
+# 			print('lowest similarity: ', min(min_y_preds))
 
-	np.save(my_system + '_min_y_preds', min_y_preds)
+# 	np.save(my_system + '_min_y_preds', min_y_preds)
+
+# save cosine similarity
 
 # for my_system in my_systems:
 # 	print(my_system)
